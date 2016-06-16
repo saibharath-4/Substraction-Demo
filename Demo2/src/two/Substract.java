9492fe5fd86d4ef2b10c4aa8ffc26d9a02687e0a
Sai Bharath <saibharath.dachepalli4@gmail.com>
2016-06-16 12:34:09 +0530

    Substraction Code

---
+++ Demo2/src/two/Substract.java
@@ -0,0 +1,14 @@
+package two;
+
+public class Substract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=10;
+		int c=a-b;
+		System.out.println(c);
+
+	}
+
+}
